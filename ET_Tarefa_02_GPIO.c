6a3c04b63811f2cfb7bd3a6357109ff24e27fc81
esantos <edcarllossantos@gmail.com>
2025-01-19 22:13:18 -0300

    Inicilizando portas GPIO

--- ET_Tarefa_02_GPIO.c
+++ ET_Tarefa_02_GPIO.c
@@ -9,3 +9,36 @@
 #define LED_GREEN 11
 #define BUZZER 21
 
+///// DECLARAÇÃO
+
+void inicializacaoLEDs();
+
+void inicializacaoBuzzer();
+
+void acionaLED_R();
+
+void acionaLED_G();
+
+void acionaLED_B();
+
+void acionarTodosLEDs();
+
+void desligaTodosLEDs();
+
+void tocaBuzzer();
+
+void interpretarComando(char comando[50]);
+
+///// DEFINIÇÃO
+
+// Função principal para inicilização das portas GPIO
+void inicializacaoLEDs() {
+  // Inicializa os pinos associados aos LEDs
+  gpio_init(LED_RED);
+  gpio_init(LED_BLUE);
+  gpio_init(LED_GREEN);
+  // Configura os pinos como saída para controlar os LEDs
+  gpio_set_dir(LED_RED, GPIO_OUT);
+  gpio_set_dir(LED_BLUE, GPIO_OUT);
+  gpio_set_dir(LED_GREEN, GPIO_OUT);
+}
